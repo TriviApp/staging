b238594f0874b89fd3e81cf171775b025cacb658
Lady Blue <bluelady.2015@yahoo.com>
2015-10-19 09:45:50 -0700

    routes update

--- routes/questions_routes.js
+++ routes/questions_routes.js
@@ -2,13 +2,13 @@ var Note = require(__dirname + '/../models/question');
 var express = require('express');
 var jsonParser = require('body-parser').json();
 var handleError = require(__dirname + '/../lib/handle_error');
-var eatAuth = require(__dirname + '/../lib/eat_auth');
 
 var questionsRoute = module.exports = exports = express.Router();
 
-questionsRoute.get('/questions', jsonParser, eatAuth, function(req, res) {
+questionsRoute.get('/questions', jsonParser, function(req, res) {
   Question.find({author: req.user.username}, function(err, data) {
     if (err) return handleError(err, res);
     res.json(data);
   });
 });
+
