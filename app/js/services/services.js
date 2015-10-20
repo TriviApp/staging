a30723d13e4b1bb34857bd20b4096bc1fe7eb174
Lady Blue <bluelady.2015@yahoo.com>
2015-10-20 10:58:43 -0700

    services.js and trivia.js connections established

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -1,3 +1,4 @@
 module.exports = function(app) {
 	require(__dirname + './auth_service')(app);
+  require(__dirname + './game_service')(app);
 };
--- app/js/trivia/trivia.js
+++ app/js/trivia/trivia.js
@@ -1,3 +1,4 @@
 module.exports = function(app) {
-
+  require('./controllers/trivia_controller')(app);
+  require('./directives/question_directive')(app);
 };	
