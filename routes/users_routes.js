2f09aaf91d0a5d938d3d317cf5d9388eb637a167
will.shull@gmail.com <will.shull@gmail.com>
2015-10-20 10:42:28 -0700

    added passport middleware to backend routes, added  interceptor to catch response errors from the server

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -2,51 +2,35 @@ var express = require('express');
 var User = require(__dirname + '/../models/user');
 var jsonParser = require('body-parser').json();
 var handleError = require(__dirname + '/../lib/handle_error');
-var httpBasic = require(__dirname + '/../lib/http_basic');
-var eatAuth = require(__dirname + '/../lib/eat_auth');
+var handleResponse = require(__dirname + '/../lib/handle_response');
+var passport = require('passport');
+var basicAuth = require(__dirname + '/../lib/strategies/passport_basic_strategy');
+var bearerAuth = require(__dirname + '/../lib/strategies/passport_bearer_strategy');
 
 var usersRouter = module.exports = exports = express.Router();
+usersRouter.use(passport.initalize());
 
 usersRouter.post('/signup', jsonParser, function(req, res) {
   var newUser = new User();
-  newUser.basic.username = req.body.username;
   newUser.username = req.body.username;
   newUser.generateHash(req.body.password, function(err, hash) {
-    if (err) return handleError(err, res);
+    if (err) return handleError.err500(err, res);
     newUser.save(function(err, data) {
-      if (err) return handleError(err, res);
+      if (err) return handleError.err500(err, res);
       newUser.generateToken(function(err, token) {
-        if (err) return handleError(err, res);
+        if (err) return handleError.err500(err, res);
         res.json({token: token});
       });
     });
   });
 });
 
-usersRouter.get('/signin', httpBasic, function(req, res) {
-  User.findOne({'basic.username': req.auth.username}, function(err, user) {
-    if (err) return handleError(err, res);
-
-    if (!user) {
-      console.log('could not authenticat: ' + req.auth.username);
-      return res.status(401).json({msg: 'could not authenticat'});
-    }
-
-    user.compareHash(req.auth.password, function(err, hashRes) {
-      if (err) return handleError(err, res);
-      if (!hashRes) {
-        console.log('could not authenticat: ' + req.auth.username);
-        return res.status(401).json({msg: 'authenticat says no!'});
-      }
-
-      user.generateToken(function(err, token) {
-        if (err) return handleError(err, res);
-        res.json({token: token});
-      });
-    });
-  });
+usersRouter.get('/signin', basicAuth.basicAuthentication, function(req, res) {
+  var user = req.user;
+  delete user.password;
+  handleResponse.send201(res, user);
 });
 
-usersRouter.get('/username', jsonParser, eatAuth, function(req, res) {
+usersRouter.get('/username', bearerAuth.bearerAuthentication, function(req, res) {
   res.json({username: req.user.username});
 });
