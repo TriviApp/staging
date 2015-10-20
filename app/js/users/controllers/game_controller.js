44d893842d5409fa2febcf86b349ef883695ded4
Molly Kent <kent.molly@gmail.com>
2015-10-20 11:25:19 -0700

    Pair programming with Clare on gameplay views

--- app/js/users/controllers/game_controller.js
+++ app/js/users/controllers/game_controller.js
@@ -1,18 +1,22 @@
 module.exports = function(app) {
   app.controller('GameController', ['$rootScope', '$scope', '$location', '$http', function($rootScope, $scope, $location, $http) {
+    $scope.questionsArrIndex = 0;
     $scope.categoryName = $rootScope.gameData.category;
-    $scope.questions = $rootScope.gameData.questions;
-    $scope.currentQuestionIndex = 0;
-    $scope.currentQuestion = '';
+    $scope.questionsArr = $rootScope.gameData.questions;
+    $scope.question = $rootScope.gameData.questions[$scope.questionsArrIndex].question;
+    $scope.answers = $rootScope.gameData.questions[$scope.questionsArrIndex].answers;
+    $scope.correctAnswer = $rootScope.gameData.questions[$scope.questionsArrIndex].correctAnswer;
+    
     var right = '';
     var wrong = '';
 
-    // get res obj, store as gameData --done in home_controller?
-    // pull category name off gameData -- done in home_controller?
-    // pull array of {question} objects off gameData --done in category_model
+    // get res obj, store as gameData -- DONE in home_controller
+    // pull category name off gameData -- DONE in line 3 
+    // pull array of {question} objects off gameData -- DONE in category_model, assigned here
     // will come in as an array of answer strings and a question:string key-value pair and correctAnswer key-value pair  
     // ng-repeat the answers to display on the buttons in the order they came in (they are shuffled on the back end)
     // display the question (duh)
+
     // on click (user choice), run checkAnswer()
     // CHECKANSWER should compare the chosen answer with the correctAnswer 
         // if true, run the counter, return true, AND add the classes "correct", "animated" and "rubberband" to the clicked button AND change the class for remaining answer-buttons to fade them out
@@ -20,7 +24,7 @@ module.exports = function(app) {
     // run nextQuestion() after a delay of X seconds
 
     $scope.nextQuestion = function() {
-      return $scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+      return $scope.question = $scope.questions[$scope.questionsArrIndex];
       // assign 'correct' and 'incorrect' classes to buttons in DOM
     };
     $scope.designateAnswer = function() {
@@ -32,7 +36,7 @@ module.exports = function(app) {
     $scope.checkAnswer = function() {
       if (answer === $rootScope
                       .gameData
-                      .questions[$scope.currentQuestionIndex]
+                      .questions[$scope.questionsArrIndex]
                       .correctAnswer) {
         right += 1;
         return true;
@@ -40,7 +44,7 @@ module.exports = function(app) {
         wrong += 1;
         return false;
       }
-      $scope.currentQuestionIndex += 1;
+      $scope.questionsArrIndex += 1;
 
     };
     $scope.showResults = function() {};
