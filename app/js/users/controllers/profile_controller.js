509159a3f8c18a71396900bb554b1b4180a65414
will.shull@gmail.com <will.shull@gmail.com>
2015-10-22 13:16:52 -0700

    added auth service to check for rootScope.user and token in sessionStorage

--- app/js/users/controllers/profile_controller.js
+++ app/js/users/controllers/profile_controller.js
@@ -1,12 +1,18 @@
 module.exports = function(app) {
-  app.controller('ProfileController', ['$rootScope', '$scope', '$location', '$http', function($rootScope, $scope, $location, $http) {
-    $scope.user = $rootScope.user;
+  app.controller('ProfileController', ['$rootScope', '$scope', '$location', '$http', 'AuthService',
+    function($rootScope, $scope, $location, $http, AuthService) {
+      if (!$rootScope.user) {
+        AuthService.restoreSession();
+        $scope.user = $rootScope.user;
+      } else {
+        $scope.user = $rootScope.user;
+      }
 
-    $scope.rankings = function(category) {
-      var correct = $scope.user.category.correct;
-      var total = $scope.user.category.total;
-      return correct/total;
-    };
+      $scope.rankings = function(category) {
+        var correct = $scope.user.category.correct;
+        var total = $scope.user.category.total;
+        return correct/total;
+      };
 
   }])
 }
