a632fe20a15912c82b1bfac2b2044044b600a12e
clareluna <clarelunamonahan@gmail.com>
2015-10-22 16:23:43 -0700

    to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || 'mongodb://heroku_vfw504rw:bjmrfolccnnhu8g37a74am7g4@ds041934.mongolab.com:41934/heroku_vfw504rw');
-process.env.APP_SECRET = process.env.APP_SECRET || 'changemechangemechangeme';
+process.env.APP_SECRET = process.env.APP_SECRET || '43e4ba116b2be2f231ce49aa496b55a22eac91dd32227d7fbf6fbbbf1969f443c2f6c89800de6f739cbcae422fac9d63';
 
 app.use(express.static(__dirname + '/build'));
 
