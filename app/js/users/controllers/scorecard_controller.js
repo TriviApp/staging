aaa0e3172847b6bedef5df6b0ed9d925929d3cd1
clareluna <clarelunamonahan@gmail.com>
2015-10-23 10:35:38 -0700

    not best

--- app/js/users/controllers/scorecard_controller.js
+++ app/js/users/controllers/scorecard_controller.js
@@ -12,36 +12,12 @@ module.exports = function(app) {
     var scoreArr = $rootScope.scoreArr;
     $scope.right = $rootScope.right;
     $scope.wrong = $rootScope.wrong;
-    // var scoreArr = [true,true,false,true,true];
 
-    var gameData = {
-      "category": "sports",
-      "questions": [
-        { "question":"Which NHL Team are nicknamed the 'Coyotes'?",
-          "answers": ["Calgary", "Vancouver", "Ottawa", "Arizona"],
-          "correctAnswer": "Arizona"},
-        { "question":"Which U.S. golfer stands second in the all-time list of major winners with thirteen titles?",
-          "answers": ["Tiger Woods", "Tony Jacklin", "Bobby Jones", "Arnold Palmer"],
-          "correctAnswer": "Bobby Jones"},
-        { "question":"Chukkas is the term given to periods played in what sport?",
-          "answers": ["Polo", "Ice Hockey", "Hockey", "Curling"],
-          "correctAnswer": "Polo"},
-        { "question":"How many goose feathers does it take to make a shuttlecock?",
-          "answers": ["16", "60", "21", "32"],
-          "correctAnswer": "16"},
-        { "question":"In Olympic Archery, how far is the competitor from the target?",
-          "answers": ["50m", "120m", "70m", "100m"],
-          "correctAnswer": "70m"}
-          ]
-        }
-    $scope.categoryName = gameData.category;
-    $scope.questionsArrIndex = 0;
-    $scope.questionsArr = gameData.questions;
-    $scope.question = gameData.questions[$scope.questionsArrIndex].question;
-    $scope.correctAnswer = gameData.questions[$scope.questionsArrIndex].correctAnswer;
+    $scope.categoryName = $rootScope.gameData.category;
+    $scope.questionsArr = $rootScope.gameData.questions;
+    $scope.question = $rootScope.gameData.questions[$scope.questionsArrIndex].question;
+    $scope.correctAnswer = $rootScope.gameData.questions[$scope.questionsArrIndex].correctAnswer;
 
-    // $scope.right = 3;
-    // $scope.wrong = 2;
     $scope.incorrectArr = [];
     $scope.correctArr = [];
 
--- routes/categories_routes.js
+++ routes/categories_routes.js
@@ -14,22 +14,7 @@ categoryRouter.use(passport.initialize());
 categoryRouter.get('/categories/:category', bearerAuth.bearerAuthentication, function (req, res) {
   Category.findOne({category: req.params.category}, function (err, data) {
     if (err) return (err, res);
-<<<<<<< HEAD
-    console.log('cat', req.params.category);
-    console.log('data pre sample', data);
-        var data = [
-	 		{"question":"Which NHL Team are nicknamed the 'Coyotes'?", "answers": ["Calgary", "Vancouver", "Ottawa", "Arizona"], "correctAnswer": "Arizona"},
-	 		{"question":"Which U.S. golfer stands second in the all-time list of major winners with thirteen titles?", "answers": ["Tiger Woods", "Tony Jacklin", "Bobby Jones", "Arnold Palmer"], "correctAnswer": "Bobby Jones"},
-	 		{"question":"Chukkas is the term given to periods played in what sport?", "answers": ["Polo", "Ice Hockey", "Hockey", "Curling"], "correctAnswer": "Polo"},
-	 		{"question":"How many goose feathers does it take to make a shuttlecock?", "answers": ["16", "60", "21", "32"], "correctAnswer": "16"},
-	 		{"question":"In Olympic Archery, how far is the competitor from the target?", "answers": ["50m", "120m", "70m", "100m"], "correctAnswer": "70m"}
-	 		];
-    var questions = _.sample(data, 2);
-=======
-    console.log('req.params', req.params);
-    console.log('data pre sample', data.questions);
     var questions = _.sample(data.questions, 5);
->>>>>>> be3a90474d8f4a8ee543f8f867ffab2c7200ce92
     console.log('questions', questions);
     res.json({msg: questions, category: req.params.category});
   });
